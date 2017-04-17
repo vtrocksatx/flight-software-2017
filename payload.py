59e8194a738ccc58159311246cb44cf5f6ed5c11
Marcus Wanner <marcus@wanners.net>
2017-04-17 15:29:16 -0400

    ADS-B packet formatting done

--- payload.py
+++ payload.py
@@ -7,7 +7,7 @@
 # nc -lp2600 | ./payload.py
 # ./mockcommand.py
 
-import os, socket, time
+import os, socket, time, struct
 from threading import Thread
 from datareporter import TCPDataReporter, UDPDataReporter
 from subprocess import Popen, PIPE
@@ -30,7 +30,8 @@ def main():
 class FlowGraphManager():
     def __init__(self):
         self.topblock = None
-        self.r = UDPDataReporter(("10.101.10.1", 1337))
+        #self.r = UDPDataReporter(("10.101.10.1", 1337))
+        self.r = UDPDataReporter(("localhost", 1337))
 
     def processinput(self):
         cmd = raw_input("PLD>> ").strip().lower()
@@ -66,15 +67,26 @@ class TopBlockThread(Thread):
     def _pad_msg(self, data):
         return data.ljust(self.r.max_msg_len, "\0")
     def _process_adsb(self, data):
-        return self._pad_msg("ADS-B RX {}\n".format(data.strip().split()[0]))
+        msg = data.strip().split()[0].decode("hex")
+        try:
+            self.adsb_part.add(msg)
+        except self.adsb_part.PacketFullError:
+            # packetize the queued messages
+            ret = str(self.adsb_part)
+            # add the next message to the next packet
+            self.adsb_part = ADSBPacker()
+            self.adsb_part.add(msg)
+            return ret
     def _process_ais(self, data):
         raise NotImplementedError("no AIS data formatter yet")
     def _process_test(self, data):
-        return self._pad_msg(data.strip())
+        return data.strip()
+
+
     mode_handlers = {
             "adsb":     _process_adsb,
             "ais":      _process_ais,
-            "testmode": _process_test,
+            "testmode": _process_adsb,
     }
     mode_binaries = {
             "adsb":     os.path.join(os.path.dirname(__file__), "modes_rx"),
@@ -87,6 +99,7 @@ class TopBlockThread(Thread):
         self.mode = mode
         self.r = r
         self.p = None
+        self.adsb_part = ADSBPacker()
         if self.mode not in self.mode_handlers.keys():
             raise ValueError("TopBlockThread mode {} not supported, try {}".format(
                     self.mode, self.mode_handlers))
@@ -100,7 +113,8 @@ class TopBlockThread(Thread):
                 if not line: # process finished
                     break
                 msg = self.mode_handlers[self.mode](self, line)
-                self.r.send(msg)
+                if msg:
+                    self.r.send(self._pad_msg(msg))
         finally:
             self.stop()
 
@@ -121,6 +135,42 @@ class TopBlockThread(Thread):
         if self.p:
             self.p.terminate()
             self.p = None
+
+
+class ADSBPacker(object):
+
+    class PacketFullError(ValueError): pass
+
+    def __init__(self):
+        self.len = 0
+        self.msgs = []
+
+    # refer to icd, ADSB section
+    def __str__(self):
+        count = len(self.msgs)
+        if count > 32: # 6 bits
+            return "\0ERROR packet too large"
+        lengths = 0
+        bit = 1
+        for i in range(count):
+            l = len(self.msgs[i])*8
+            if l == 56:
+                # 0, do nothing
+                pass
+            elif l == 112:
+                # 1, add to bitmask
+                lengths += bit
+            else:
+                raise ValueError("bad packet length {} (data {})".format(l, repr(self.msgs[i])))
+            bit *= 2
+        ret = struct.pack("!BI", count, lengths) + "".join(self.msgs)
+        return ret
+
+    def add(self, msg):
+        if (1 + 4 + self.len + len(msg)) > 252:
+            raise self.PacketFullError("packet is full!")
+        self.msgs.append(msg)
+        self.len += len(msg)
 
 
 if __name__ == "__main__":
--- testmode/top_block.py
+++ testmode/top_block.py
@@ -2,7 +2,14 @@
 
 import sys, time
 
-for i in range(100):
-    print(hex(int(time.time()*100+i)))
+i = 0
+while 1:
+    pkt = hex(int(time.time()*10+i))[-4:]
+    if int(time.time()*16) % 2 != 1:
+        pkt = pkt.rjust(56/8, "s")
+    else:
+        pkt = pkt.rjust(112/8, "l")
+    print(pkt.encode("hex"))
     sys.stdout.flush()
-    time.sleep(0.2)
+    time.sleep(0.1)
+    i += 1
